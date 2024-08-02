5580ea683d7b66b472f0fbb8cbe1fd31d6cb4713
MugaRay <160254607+MugaRay@users.noreply.github.com>
2024-08-02 13:22:53 +0200

    prism and compose model not done

---
+++ elm-tutorial/src/Main.elm
@@ -0,0 +1,192 @@
+module Main exposing (..)
+
+
+import Browser 
+import Html exposing (Html, button, div, text, ol, li)
+import Html.Events exposing (onClick)
+import Dict exposing (update)
+import List exposing (map)
+import Html.Attributes exposing (value)
+
+
+
+main = 
+    Browser.sandbox {init = init, update=update ,view=view}
+
+
+
+-- Types
+type alias ComprehensiveRecord  =
+    { penaltyAmount : Float
+    , email : String
+    , name: String
+    }
+
+type alias BasicRecord = 
+    {
+        name: String
+    ,   email : Maybe String
+    }
+
+
+type PerUserData 
+    = ComprehensiveRecordData ComprehensiveRecord
+    | BasicData BasicRecord 
+
+
+-- Lens and prisms
+
+type alias Lens structure value = 
+    {
+        get : structure -> value 
+    ,   set : structure -> value -> structure 
+    }
+
+type alias Prism structure value = 
+    {   get: structure -> Maybe value
+    ,   set: structure -> value -> structure 
+    }
+
+
+-- Lens and prism implementations
+
+getBasicName: Lens BasicRecord String
+getBasicName = 
+    {
+        get = \structure -> structure.name
+    ,   set = \structure value -> {structure | name= value}
+    }
+
+getBasicEmail: Prism BasicRecord String 
+getBasicEmail = 
+    {
+        get = \structure -> 
+            case structure.email of 
+                Nothing -> Nothing
+                Just a -> Just a
+    ,   set = \structure value -> 
+                    {structure | email= Just value}
+    }
+
+
+
+getComprehensiveName: Lens ComprehensiveRecord String 
+getComprehensiveName =
+    {
+        get = \structure -> structure.name
+    ,   set = \structure value -> {structure | name = value}
+    }
+
+
+getComprehensiveEmail: Lens ComprehensiveRecord String 
+getComprehensiveEmail =
+    {
+        get = \structure -> structure.email
+    ,   set = \structure value -> {structure | email = value}
+    }
+
+
+
+getComprehensivePenalty: Lens ComprehensiveRecord Float 
+getComprehensivePenalty =
+    {
+        get = \structure -> structure.penaltyAmount
+    ,   set = \structure value -> {structure | penaltyAmount = value}
+    }
+
+
+getPerUserName: Lens PerUserData String
+getPerUserName = 
+    {
+        get =  \structure -> 
+            case structure of 
+                ComprehensiveRecordData v -> v.name 
+                BasicData v -> v.name
+    ,   set = \structure value -> 
+            case structure of 
+                ComprehensiveRecordData v -> ComprehensiveRecordData {v | name = value} 
+                BasicData v -> BasicData {v | name = value}
+    }
+
+
+
+getComprehensiveRecord: PerUserData -> Maybe ComprehensiveRecord
+getComprehensiveRecord userData 
+    = case userData of
+        BasicData _ -> Nothing
+        ComprehensiveRecordData v -> Just v
+
+
+
+randomName = BasicData {
+        name = "carl"
+    ,   email = Just "Hello"
+    }
+
+
+
+
+
+-- Model 
+type alias Model =
+    {   showing: String
+    ,   students : List String
+    ,   perUserData : Maybe PerUserData
+    }
+
+init : Model
+init =  
+    {
+        showing = ""
+    ,   students = ["carl", "manny", "Lusi"]
+    ,   perUserData = Just randomName
+    }
+
+
+
+
+
+
+
+-- Update
+
+type Msg
+    = Name
+    | NoShow 
+
+update: Msg -> Model -> Model
+update msg model =
+    case msg of 
+        Name -> model 
+        NoShow -> model
+
+
+
+
+
+
+-- View 
+
+view : Model -> Html Msg
+view model =
+  div []
+    [
+        studentList model
+    ,   button [] [text "Get name"]
+    ,   button [] [text "Get email"]
+    ,   button [] [text "Get Penalty Amount"]
+    ,   button [] [text "Get Comprehensive Record"]
+    ]
+
+
+createList : String -> Html msg
+createList item =
+    li [] [text item]
+
+buildList : List String -> List (Html msg)
+buildList lst = 
+    map createList lst
+
+studentList : Model -> Html Msg 
+studentList model = 
+    ol [] (buildList model.students)
